d0f9fce8326643995ceaedc004517e95c1f73562
Amrita-Soney <116264186+Amrita-Soney@users.noreply.github.com>
2024-03-30 22:53:00 +0530

    Add files via upload

---
+++ R1.R
@@ -0,0 +1,51 @@
+print("hello")
+2+3
+sum1
+sum2<-4+9
+sum2
+print(sum2)
+#  this is rstudio
+demo; demo; sum
+x<-1:10
+plot(x)
+#R is a case sensitive language
+
+
+#VARIABLE 
+# A Variable naming can start with a letter or a dot but not underscore in the starting.
+#only dot and underscore are the spical char allowed in var namimg
+var.name<- 8
+var_name<-2
+_var<-3
+.var<-4
+var*<-4
+
+#assignment of variables
+v<-1
+1<-v1
+1->v1
+v2=1
+#v<-2 use this. it is universal
+cat(v, " ",v1,"\n",v2)
+#DATA TYPES
+num<-98.78
+class(num)
+typeof(num)
+#numeric datatypes double me hi store hote hn
+# Two ways of converting numeric double into integer 
+num2<-23
+class(num2)
+num2<-as.integer(num2)
+class(num2)
+#or
+num3<-1L
+class(num3)
+typeof(num3) 
+typeof(num2)
+
+x<-as.integer(TRUE)
+x
+x1<-as.numeric(56-23i)
+x1
+x2<-as.integer(54-23i)
+x2
